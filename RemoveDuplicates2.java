5f708b554356a97558fa7c1409ecf3805f695728
Chetan Khilare <chetan.kh23@gmail.com>
2025-01-15 19:58:37 -0500

    Completed 3rd problem

---
+++ RemoveDuplicates2.java
@@ -0,0 +1,26 @@
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+public class RemoveDuplicates2 {
+  public int removeDuplicates(int[] nums) {
+    int count = 1;
+    int index = 1;
+
+    for (int i = 1; i < nums.length; i++) {
+      if (nums[i] == nums[i - 1]) {
+        count++;
+      } else {
+        count = 1;
+      }
+
+      if (count <= 2) {
+        nums[index] = nums[i];
+        index++;
+      }
+    }
+
+    return index;
+  }
+}
